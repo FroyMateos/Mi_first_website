857fa7803556714f2b46d73c7db0c565d9de8290
Froylan Mateos <mateosfroylan@gmail.com>
2020-12-08 22:08:04 -0600

    segunda subida

--- index.py
+++ index.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/proyectos')#localhost:5000/about
+@app.route('/proyectos')
 def proyectos():
     return render_template('proyectos.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
 
 if __name__=='__main__':
     app.run(debug=True) #Para modo prueba (no sera necesario cerrar el programa)
